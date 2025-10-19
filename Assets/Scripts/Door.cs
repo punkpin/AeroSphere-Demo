95acd591ca2b676bd02a61730da7d1a5bda72fe0
qui-rrel <430534193@qq.com>
2025-10-19 21:02:06 +0800

    空气墙

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite endDoor;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameManager.instance.currentLevel.EndLevel();
+        spriteRenderer.sprite = endDoor;
     }
 }
